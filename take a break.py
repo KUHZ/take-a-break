7584efe691321e12ed430b16c9d001517a80b5c1
GHZ <kuhzcn@gmail.com>
2017-10-03 10:43:05 +0800

    inital commit little program take a break

---
+++ take a break.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+breaktime = 3
+count =0
+print("the program started on "+time.ctime())
+while(count < breaktime):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=-qlJiGGvPUI")
+    count = count + 
